1610e89d0368617e463ca09be01993911fab42a9
chandralekhasingasani <chandralekha.s@nutanix.com>
2023-06-13 09:16:44 +0530

    test

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,7 @@ output "TGW_DEFAULT_ATTACHMENT_ID" {
 output "TGW_DEFAULT_ROUTE_TABLE_ID" {
   value = aws_ec2_transit_gateway_route_table.default-vpc.id
 }
+
+output "PUBLIC_ROUTE_TABLE_ID_DEFAULT_VPC" {
+  value = "rtb-0e8c3be672fa81ebb"
+}
